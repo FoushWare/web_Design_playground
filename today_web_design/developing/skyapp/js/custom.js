fd1ea4b1b07e52c9f36d065fdb036669702ad226
lionmoon60 <lionmoon60@yahoo.com>
2016-09-22 00:22:29 +0200

    second commit of sass&bootstrap project

---
+++ today_web_design/developing/skyapp/js/custom.js
@@ -0,0 +1,4 @@
+$(function(){
+   $('#home-heading').fitText(2, { minFontSize: '20px', maxFontSize: '30px' });
+   $('.section-primary-a h2').fitText(3, { minFontSize: '20px', maxFontSize: '30px' });
+});
